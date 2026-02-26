ca0ce99ba23dfc45883bda7028b44cd9ceb30b35
LinusHell <linus.hellebrandt@gmx.de>
2026-02-26 23:50:42 +0100

    Declocked fps and added circleshape parent class

--- main.py
+++ main.py
@@ -8,8 +8,12 @@ from logger import log_state
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}",f"Screen height: {SCREEN_HEIGHT}")
-    pygame.init()
+    pygame.init()  #Initialises pygame
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    game_clock = pygame.time.Clock() 
+    dt = 0
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -17,6 +21,9 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+
+        #Let 1/60 th of a second pass, and records the actual time that passed:
+        dt = game_clock.tick(60) / 1000 
     
 
 
