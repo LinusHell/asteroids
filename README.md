01fb8dfa3d79d9db7de90be634d751b19e6997a4
LinusHell <linus.hellebrandt@gmx.de>
2026-02-27 09:54:50 +0100

    Added player, asteroid and asteroidfield class, and grouped drawables and updatables.

---
+++ README.md
@@ -0,0 +1,8 @@
+This project builds a simple Asteroid game using the instructions on the corresponding course on boot.dev
+
+Possibly this project will be extended after finishing the course with additional features.
+
+
+
+
+# To restart the virtual enviroment use ".venv/bin/activate
--- main.py
+++ main.py
@@ -4,15 +4,32 @@ os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}",f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()  #Initialises pygame
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     game_clock = pygame.time.Clock() 
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable,drawable)
+
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+
+    AsteroidField.containers = (updatable,)
+
+    asteroidfield = AsteroidField()
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
@@ -20,6 +37,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+
+        updatable.update(dt)
+
+        for item in drawable:
+            item.draw(screen)
+
         pygame.display.flip()
 
         #Let 1/60 th of a second pass, and records the actual time that passed:
